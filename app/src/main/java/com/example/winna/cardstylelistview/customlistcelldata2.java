a4c40fd577691e146a153fecc9578b7e451459f2
winnawen <winna@winnadeMacBook-Pro.local>
2016-10-17 18:19:49 +0800

    shop

---
+++ app/src/main/java/com/example/winna/cardstylelistview/customlistcelldata2.java
@@ -0,0 +1,13 @@
+package com.example.winna.cardstylelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class customlistcelldata2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customlistcelldata2);
+    }
+}
